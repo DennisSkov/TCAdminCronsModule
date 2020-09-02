c31337a0e1f2aef3cb4d1d7fcc2bcecb4d091947
Alex Redding <Alexredding180@gmail.com>
2020-09-02 16:49:25 +0100

    Remove configuration from tc_info on uninstall

--- uninstall.sql
+++ uninstall.sql
@@ -3,3 +3,4 @@ DELETE FROM tc_site_map WHERE module_id LIKE '3a0e1e17-cbee-4e00-871e-e3f492e8c8
 DELETE FROM tc_panelbar_categories WHERE module_id LIKE '3a0e1e17-cbee-4e00-871e-e3f492e8c8da';
 DELETE FROM tc_module_server_components WHERE module_id LIKE '3a0e1e17-cbee-4e00-871e-e3f492e8c8da';
 DELETE FROM tc_server_enabled_components WHERE module_id LIKE '3a0e1e17-cbee-4e00-871e-e3f492e8c8da';
+DELETE FROM tc_info WHERE name LIKE 'Crons.MinecraftUpdates';
